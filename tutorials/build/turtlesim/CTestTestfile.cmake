eb56d4a7f1b628a5042daf440504c8f598cc13ff
chenlehui <chenlehui@example.com>
2022-01-05 12:47:06 +0800

    01

---
+++ tutorials/build/turtlesim/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/cclehui/go_workspace/src/github.com/cclehui/study_ros/tutorials/src/turtlesim
+# Build directory: /home/cclehui/go_workspace/src/github.com/cclehui/study_ros/tutorials/build/turtlesim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("turtlesim__py")
